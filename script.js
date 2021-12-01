12d7559af5cde6ccbf7b322c7ad6a915668c66fe
k3L0w <developer.criation@gmail.com>
2021-12-01 19:47:14 -0300

    :package: menu sidebar

---
+++ script.js
@@ -0,0 +1,3 @@
+document.querySelector(".burguer").addEventListener("click", () => //ao 'ouvir' click executará a função
+    document.querySelector(".container").classList.toggle("show-menu") //adicionar ou remover uma classe que se chama show-menu
+);
